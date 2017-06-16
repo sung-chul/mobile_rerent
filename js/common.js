58bfd24953d09d4f613215b4be5a9b2537d900e6
SUHYUN\grayblue <suhyun8210@gmail.com>
2017-06-16 16:11:47 +0900

    no message

--- js/common.js
+++ js/common.js
@@ -14,9 +14,38 @@ $(window).load(function(){
 	$('.header .btn-close').click(function(){
 		window.close();
 	});
-	
+
 	/* 검색결과 출력 */
 	$('.gray-bg-box .btn-result').click(function(){
 		$('.rentcar-result').show();
 	});
+
+	/* ===========================================================================================================
+		레이어 팝업
+	=========================================================================================================== */
+	$('.layer-popup, .layer-menu').hide().css({'opacity':'1'});
+	$('.layer-popup, .layer-menu').each(function(){
+		var winH = $(window).height();
+		var popH = $(this).outerHeight();
+
+		if(popH >= winH){
+			$(this).addClass('scroll');
+		}else{
+			$(this).removeClass('scroll');
+		}
+	});
+
+	$('.btn-popup').click(function(){
+		var zIdx = $('.layer-popup').css('z-index');
+		$('body').append('<div class="popup-close">');
+		$('.popup-close').css({'z-index':zIdx-1});
+		$('body').css({'overflow':'hidden'});
+	});
+	$(document).on('click', '.layer-popup .btn-close, .popup-close, .layer-menu .btn-close', function(){
+		$('.layer-popup, .layer-menu').fadeOut(200);
+		$('.bg-layer').fadeOut(200);
+		$('.popup-close').remove();
+		$('body').css({'overflow-y':'inherit'});
+	});
+
 });
